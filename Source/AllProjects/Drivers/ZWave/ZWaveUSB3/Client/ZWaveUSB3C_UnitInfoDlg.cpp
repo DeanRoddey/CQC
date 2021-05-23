5a7652f640ac3ef0e168369ec1e4e2e549941693
Dean Roddey <droddey@charmedquark.com>
2021-05-23 16:57:14 -0400

    One small fix required for CIDLib changes just pushed

--- Source/AllProjects/Drivers/ZWave/ZWaveUSB3/Client/ZWaveUSB3C_UnitInfoDlg.cpp
+++ Source/AllProjects/Drivers/ZWave/ZWaveUSB3/Client/ZWaveUSB3C_UnitInfoDlg.cpp
@@ -7,8 +7,8 @@
 //
 // COPYRIGHT: Charmed Quark Systems, Ltd @ 2020
 //
-//  This software is copyrighted by 'Charmed Quark Systems, Ltd' and 
-//  the author (Dean Roddey.) It is licensed under the MIT Open Source 
+//  This software is copyrighted by 'Charmed Quark Systems, Ltd' and
+//  the author (Dean Roddey.) It is licensed under the MIT Open Source
 //  license:
 //
 //  https://opensource.org/licenses/MIT
@@ -483,9 +483,9 @@ tCIDLib::TVoid TZWUSB3UnitInfoDlg::SaveInfo()
     if (bRes)
     {
         // If it still has any of the replacement values, reject it
-        if (strSelected.bContainsSubStr(L"[yourname]")
-        ||  strSelected.bContainsSubStr(L"[make]")
-        ||  strSelected.bContainsSubStr(L"[model]"))
+        if (strSelected.bContainsSubStr(L"[yourname]", kCIDLib::False)
+        ||  strSelected.bContainsSubStr(L"[make]", kCIDLib::False)
+        ||  strSelected.bContainsSubStr(L"[model]", kCIDLib::False))
         {
             TErrBox msgbErr
             (
